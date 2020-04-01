ca193b81a297bc3a98f1bb07d215cc6088d75506
luisanegri <luisaanegri@gmail.com>
2020-04-01 19:03:05 +0200

    user auth

--- leadmanager/leads/api.py
+++ leadmanager/leads/api.py
@@ -5,39 +5,17 @@ from .serializers import LeadSerializer
 # Lead Viewset
 
 
-# class LeadViewSet(viewsets.ModelViewSet):
-
-#     permission_classes = [
-#         permissions.AllowAny
-#     ]
-
-#     serializer_class = LeadSerializer
-
-#     # methods
-
-#     def get_queryset(self):
-#         # get the leads only from the user logged
-#         return self.request.user.leads.all()
-
-#     # it allows to save the lead owner when we create the lead
-#     def perform_create(self, serializer):
-#         serializer.save(owner=self.request.user)
-
 class LeadViewSet(viewsets.ModelViewSet):
-
-    queryset = Lead.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
 
     serializer_class = LeadSerializer
 
-# methods
+    def get_queryset(self):
+        # get the leads only from the user logged
+        return self.request.user.leads.all()
 
-# def get_queryset(self):
-#     # get the leads only from the user logged
-#     return self.leads.all()
-
-# it allows to save the lead owner when we create the lead
-# def perform_create(self, serializer):
-#     serializer.save(owner=self.request.user)
+    # it allows to save the lead owner when we create the lead
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
