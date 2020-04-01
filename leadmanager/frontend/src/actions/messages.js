1ae4da8e60b002d945085e3d7eae49a27f225411
luisanegri <luisaanegri@gmail.com>
2020-04-01 19:05:02 +0200

    function to return errors dispatching in catch

--- leadmanager/frontend/src/actions/messages.js
+++ leadmanager/frontend/src/actions/messages.js
@@ -1,9 +1,17 @@
-import { CREATE_MESSAGE } from './types';
+import { CREATE_MESSAGE, GET_ERRORS } from './types';
 
 // not doing assynchronous call so no need to dispatch anything
 export const createMessage = msg => {
   return {
     type: CREATE_MESSAGE,
     payload: msg
+  };
+};
+
+export const returnErrors = (msg, status) => {
+  return {
+    type: GET_ERRORS,
+    // pass an object with msg and status
+    payload: { msg, status }
   };
 };
